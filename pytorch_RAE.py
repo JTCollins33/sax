e77acbb695d19f1b4e96b0c9cd4a910404df4613
JTCollinsy99@gmail.com <64708923+JTCollins33@users.noreply.github.com>
2021-05-11 09:32:38 -0400

    Update pytorch_RAE.py

--- pytorch_RAE.py
+++ pytorch_RAE.py
@@ -10,72 +10,96 @@ import time
 from layers_gather import get_layers, get_validation_layers
 from layer import Layer
 import random
+from torch.autograd import Variable
+import torch.autograd
 
 
 dir_path = "./data/"
-fpath = "./data/featurization_figs/RAE/new/"
-models_path = "./pytorch_model_progress/"
-N_CLUSTERS=4
+fpath = "./LSTM_results/"
+models_path = "./pytorch_model_progress/2-2_deeper_adaptive/"
+N_CLUSTERS=3
 num_epochs=100
 save_freq=2
 batch_size=15
 lr = 0.001
 N_FEATURES=8
-training=True
-testing=False
+training=False
+testing=True
 
 
 class REncoder(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+    def __init__(self, num_features, hidden_size, num_layers, num_classes, device):
         super(REncoder, self).__init__()
-        # self.r1 = nn.GRU(input_size=None, hidden_size=None, batch_first=True)
-        # self.r2 = nn.GRU(input_size=None, hidden_size=None, batch_first=True)
-        self.emb = nn.Embedding(num_embeddings=20, embedding_dim=input_size)
-        self.r1 = nn.LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=True)
-        self.lin1 = nn.Linear(hidden_size, num_classes)
+        self.hidden_size=hidden_size
+        self.device = device
+        self.r1 = nn.GRU(input_size=num_features, hidden_size=hidden_size, num_layers=num_layers, batch_first=True)
+        self.r2 = nn.GRU(input_size=hidden_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=True)
+        self.m = nn.AdaptiveMaxPool2d((1,num_classes))
+        # self.emb = nn.Embedding(num_embeddings=20, embedding_dim=input_size)
+        # self.r1 = nn.LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=True)
+        # self.lin1 = nn.Linear(hidden_size, num_classes)
 
-    def forward(self, input):
-        x = self.emb(input)
-        x = torch.reshape(x, (x.shape[0], x.shape[1], x.shape[3]))
-        x, (h_n, h_c) = self.r1(x, None)
-        x = self.lin1(x[:, -1, :])
-        return x
+    def forward(self, input, lens, hidden):
+        # x = nn.utils.rnn.pack_padded_sequence(input, lens, batch_first=True, enforce_sorted=False)
+        x, hidden = self.r1(input, None)
+        x, hidden = self.r2(x, hidden)
+        # x, _ = nn.utils.rnn.pad_packed_sequence(x, batch_first=True)
+        # x = x[:,-1,:]
+        # out = self.lin1(x)
+        out = self.m(x).squeeze()
+        return out, hidden
 
 class RDecoder(nn.Module):
-    def __init__(self, output_size, hidden_size, num_layers, num_classes):
+    def __init__(self, output_size, hidden_size, num_layers, num_classes, device):
         super(RDecoder, self).__init__()
-        self.r1 = nn.LSTM(input_size=num_classes, hidden_size=hidden_size, num_layers=num_layers, batch_first=True)
-        self.r2 = nn.LSTM(input_size=hidden_size, hidden_size=output_size, num_layers=num_layers, batch_first=True)
+        self.hidden_size=hidden_size
+        self.device = device
+        self.r1 = nn.GRU(input_size=num_classes, hidden_size=hidden_size, num_layers=num_layers, batch_first=True)
+        self.r2 = nn.GRU(input_size=hidden_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=True)
+        self.lin1 = nn.Linear(hidden_size, output_size)
 
 
-    def forward(self, input, dim):
-        # with torch.no_grad():
-        #     x = nn.Linear(N_FEATURES, dim)(input)
-        #     x = nn.Linear(dim, dim*N_FEATURES)(x)
-
+    def forward(self, input, dim, hidden):
         #simulate repeat_vector
-        x = torch.zeros([input.shape[0], dim, input.shape[1]], dtype = input[0][0].dtype)
+        x = torch.zeros([input.shape[0], dim, input.shape[1]], dtype = input[0][0].dtype).to(self.device)
         for i in range(x.shape[0]):
             for j in range(dim):
                 x[i][j] = input[i]
 
-        # x = torch.reshape(x, (x.shape[0], dim, N_FEATURES))
-        x, (h_n, h_c) = self.r1(x, None)
-        x, (h_n, h_c) = self.r2(x, None)
-        return x
+        x, hidden = self.r1(x, hidden)
+        x, hidden = self.r2(x, hidden)
+        out = self.lin1(x)
+        return out, hidden
+
+
 
 class RAE(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size, num_layers, num_classes):
+    def __init__(self, input_size, hidden_size, output_size, num_layers, num_classes, device):
         super(RAE, self).__init__()
-        self.encoder = REncoder(input_size, hidden_size, num_layers, num_classes)
-        self.decoder = RDecoder(output_size, hidden_size, num_layers, num_classes)
+        self.hidden_size=hidden_size
+        self.device = device
+        self.encoder = REncoder(input_size, hidden_size, num_layers, num_classes, device)
+        self.decoder = RDecoder(output_size, hidden_size, num_layers, num_classes, device)
 
-    def forward(self, input, dim):
-        features = self.encoder(input)
-        output = self.decoder(features, dim)
-        return output
+    def forward(self, input, dim, lens, hidden):
+        features, encoder_hidden = self.encoder(input, lens, hidden)
+        #only keep last hidden layer
+        decoder_hidden = encoder_hidden[:1].clone()
+        output, decoder_hidden = self.decoder(features, dim, decoder_hidden)
+        return output.to(self.device), decoder_hidden[:1].to(self.device)
+
+    def initHidden(self):
+        # return Variable(torch.zeros([1, batch_size, self.hidden_size], dtype=torch.float), requires_grad=True).to(self.device)
+        return Variable(torch.zeros([1, batch_size, self.hidden_size], dtype=torch.float), requires_grad=True)
 
 
+#perform kmeans clustering
+def km(layer):
+    print("Performing clustering....\n")
+    layer.kmeans_AE(clusters=N_CLUSTERS)
+
+    print("Plotting clustering....\n")
+    layer.plot_kmeans(max_clusters=N_CLUSTERS)
 
 
 #perform agglomerative clustering
@@ -95,7 +119,6 @@ def dt(layer):
     print("Plotting frequency graphs....\n")
     layer.plot_kmeans_freqs(fpath+"kmeans_frequencies/")
 
-
 def get_max(layers):
     max_vals = [0]*len(layers)
     for i in range(len(layers)):
@@ -104,8 +127,6 @@ def get_max(layers):
                 max_vals[i]=np.amax(th.curve)
 
     return int(max(max_vals))
-
-
 
 def prepare_dataset(layers):
     all_layers=[]
@@ -138,7 +159,22 @@ def get_newest_model():
     paths = [os.path.join(models_path, basename) for basename in files]
     return max(paths, key=os.path.getctime)
 
+def sort_batch(layers):
+    sorted = []
+    while (len(layers)>0):
+        max_length=0
+        max_index=-1
+        for i in range(len(layers)):
+            if(len(layers[i].curve)>max_length):
+                max_length = len(layers[i].curve)
+                max_index = i
+        sorted.append(layers.pop(max_index))
+    return sorted
+
 if __name__ == "__main__":
+    # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    device=torch.device('cpu')
+
     #get layer
     layers = get_layers(dir_path, fpath, resize=False)
 
@@ -149,9 +185,13 @@ if __name__ == "__main__":
     """
     Define Model
     """
-    model = RAE(5, 5, 1, 1, N_FEATURES)
+#     model = RAE(1, 5, 1, 1, N_FEATURES, device).to(device)
+    # model = torch.load(get_newest_model())
+    model = torch.load(models_path+"model_80_epochs", map_location=torch.device('cpu'))
     criterion = nn.MSELoss()
     optim = optim.Adam(model.parameters(), lr=lr)
+
+    hidden=model.initHidden()
 
 
     """
@@ -178,10 +218,14 @@ if __name__ == "__main__":
                         batch_base-=1
                         done=True
 
-                    ths_batch = layer.curves[batch_base:batch_base+batch_size]
+                    # ths_batch = layer.curves[batch_base:batch_base+batch_size]
+                    ths_batch= sort_batch(layer.curves[batch_base:batch_base+batch_size])
                     batch_list = []
+                    flipped_batch_list=[]
+                    seq_lens = []
                     max_length = 0
                     for th in ths_batch:
+                        seq_lens.append(len(th.curve))
                         if (len(th.curve)>max_length):
                             max_length = len(th.curve)
 
@@ -191,20 +235,24 @@ if __name__ == "__main__":
                         zeros = np.zeros(max_length-len(th.curve), dtype=th.curve.dtype)
                         padded_arr = np.append(th.curve, zeros)
                         normalized_arr = np.divide(padded_arr, 1.0*curve_max)
-                        int_arr = normalized_arr.astype(np.int64)
-                        batch_list.append(int_arr)
-                    # batch = np.array(batch_list)
+                        # int_arr = normalized_arr.astype(np.int64)
+                        flipped_batch_list.append(np.flip(normalized_arr, axis=0))
+                        batch_list.append(normalized_arr)
                     batch = torch.tensor(batch_list)
 
-                    # output_batch = batch.reshape(batch.shape[0], batch.shape[1], 1)
-                    batch = batch.reshape(batch.shape[0], batch.shape[1], 1)
+                    batch = batch.reshape(batch.shape[0], batch.shape[1], 1).to(device)
+                    flipped_batch=torch.tensor(flipped_batch_list)
+                    flipped_batch = flipped_batch.reshape(flipped_batch.shape[0], flipped_batch.shape[1], 1).to(device)
 
                     #pass into model
-                    output = model(batch, batch.shape[1])
+#                     output, hidden = model(batch, batch.shape[1], torch.tensor(seq_lens), hidden)
+                    output, hidden = model(flipped_batch.to(device), flipped_batch.shape[1], torch.tensor(seq_lens).to(device), hidden.to(device))
+
 
                     #compute loss and backpropagate
                     loss = criterion(batch, output)
-                    loss.backward()
+                    loss.backward(retain_graph=True)
+                    torch.nn.utils.clip_grad_norm_(model.parameters(), 4)
                     optim.step()
                     optim.zero_grad()
 
@@ -234,4 +282,54 @@ if __name__ == "__main__":
     Start Testing
     """
     if(testing):
-        x=2
+        for i in range(len(layers)):
+            layer = layers[i]
+            batch_base=0
+            done = False
+            layer_features = []
+
+            #create batch for testing
+            while(not(done)):
+                while(batch_base+batch_size>=len(layer.curves)):
+                    batch_base-=1
+                    done=True
+
+                # ths_batch = layer.curves[batch_base:batch_base+batch_size]
+                # ths_batch= sort_batch(layer.curves[batch_base:batch_base+batch_size])
+                ths_batch = layer.curves[batch_base:batch_base+batch_size]
+                batch_list = []
+                flipped_batch_list=[]
+                seq_lens = []
+                max_length = 0
+                for th in ths_batch:
+                    seq_lens.append(len(th.curve))
+                    if (len(th.curve)>max_length):
+                        max_length = len(th.curve)
+
+                #make curves in batch fixed length and normalize them
+                for th in ths_batch:
+                    curve_max = np.amax(th.curve)
+                    zeros = np.zeros(max_length-len(th.curve), dtype=th.curve.dtype)
+                    padded_arr = np.append(th.curve, zeros)
+                    normalized_arr = np.divide(padded_arr, 1.0*curve_max)
+                    # int_arr = normalized_arr.astype(np.int64)
+                    flipped_batch_list.append(np.flip(normalized_arr, axis=0))
+                    batch_list.append(normalized_arr)
+                batch = torch.tensor(batch_list)
+                flipped_batch = torch.tensor(flipped_batch_list)
+
+                # batch = batch.reshape(batch.shape[0], batch.shape[1], 1).to(device)
+                batch = batch.reshape(batch.shape[0], batch.shape[1], 1)
+                flipped_batch=flipped_batch.reshape(flipped_batch.shape[0], flipped_batch.shape[1], 1).to(device)
+
+                batch_features, hidden = model.encoder(flipped_batch, torch.tensor(seq_lens).to(device), hidden)
+
+                for j in range(batch_features.shape[0]):
+                    layer_features.append(batch_features[j].cpu().detach().numpy())
+
+                #move to next batch
+                batch_base+=batch_size
+            layers[i].set_dt_features(layer_features)
+
+            dt(layers[i])
+            # km(layers[i])
